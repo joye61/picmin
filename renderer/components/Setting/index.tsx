70ff288cbd1618859dbfa19ad9c5073b161b9162
Joye <89065495@qq.com>
2022-03-28 23:33:32 +0800

    update dialog

--- renderer/components/Setting/index.tsx
+++ renderer/components/Setting/index.tsx
@@ -5,12 +5,12 @@ import { Input } from "../Input";
 import { Slider } from "../Slider";
 import { Tab } from "../Tab";
 import { type ModeType, scaleMenus, state } from "@/state";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { Button } from "../Button";
 import { observer } from "mobx-react-lite";
 import { reCompress } from "@/utils";
 
-type Cstate = {
+export type Cstate = {
   mode: ModeType;
   percent: number;
   width?: string;
@@ -18,33 +18,20 @@ type Cstate = {
   quality: number;
 };
 
-export const Setting = observer(() => {
-  const boxRef = useRef<HTMLDivElement | null>(null);
-  const [animation, setAnimation] = useState<"show" | "hide">("show");
+export interface SettingOption extends Cstate {
+  onClose?: () => void;
+  onApply?: (state: Cstate) => void;
+}
 
-  useEffect(() => {
-    if (!state.showSetting) {
-      setAnimation("show");
-    }
-  }, [state.showSetting]);
-
+export const Setting = observer((props: SettingOption) => {
+  const { mode, percent, width, height, quality, onClose, onApply } = props;
   const [cstate, setCstate] = useState<Cstate>({
-    mode: "percent",
-    percent: 100,
-    width: undefined,
-    height: undefined,
-    quality: 70,
+    mode,
+    percent,
+    width,
+    height,
+    quality,
   });
-
-  let containerClass = style.container;
-  let boxClass = style.box;
-  if (animation === "show") {
-    containerClass += ` ${style.containerShow}`;
-    boxClass += ` ${style.boxShow}`;
-  } else if (animation === "hide") {
-    containerClass += ` ${style.containerHide}`;
-    boxClass += ` ${style.boxHide}`;
-  }
 
   const showModeItem = () => {
     if (cstate.mode === "percent") {
@@ -111,96 +98,70 @@ export const Setting = observer(() => {
     setCstate({ ...cstate });
   };
 
-  const onApply = () => {
-    state.mode = cstate.mode;
-    state.percent = cstate.percent;
-    state.width = cstate.width ? Number(cstate.width) : undefined;
-    state.height = cstate.height ? Number(cstate.height) : undefined;
-    state.quality = cstate.quality;
-    // 关闭弹框
-    setAnimation("hide");
-    // 应用更新
-    reCompress();
-  };
+  return (
+    <div className={style.box}>
+      {/* 关闭按钮 */}
+      <div
+        className={style.close}
+        onClick={() => {
+          onClose?.();
+        }}
+      >
+        <Icon type="close" />
+      </div>
 
-  if (state.showSetting) {
-    return (
-      <div className={containerClass}>
-        <div
-          className={boxClass}
-          ref={boxRef}
-          onAnimationEnd={(event) => {
-            if (event.animationName === style.BoxHide) {
-              state.showSetting = false;
+      <div className={style.item}>
+        <RowStart className={style.title}>设置缩放模式</RowStart>
+        <Tab
+          list={scaleMenus}
+          value={cstate.mode}
+          onChange={(value) => {
+            cstate.mode = value as ModeType;
+            if (value === "percent") {
+              cstate.width = undefined;
+              cstate.height = undefined;
+            } else if (value === "width") {
+              cstate.percent = 100;
+              cstate.height = undefined;
+            } else {
+              cstate.percent = 100;
+              cstate.width = undefined;
             }
+            setCstate({
+              ...cstate,
+            });
           }}
-        >
-          {/* 关闭按钮 */}
-          <div
-            className={style.close}
-            onClick={() => {
-              setAnimation("hide");
-            }}
-          >
-            <Icon type="close" />
-          </div>
+        />
+      </div>
 
-          <div className={style.item}>
-            <RowStart className={style.title}>设置缩放模式</RowStart>
-            <Tab
-              list={scaleMenus}
-              value={cstate.mode}
-              onChange={(value) => {
-                cstate.mode = value as ModeType;
-                if (value === "percent") {
-                  cstate.width = undefined;
-                  cstate.height = undefined;
-                } else if (value === "width") {
-                  cstate.percent = 100;
-                  cstate.height = undefined;
-                } else {
-                  cstate.percent = 100;
-                  cstate.width = undefined;
-                }
+      {/* 调整缩放模式 */}
+      {showModeItem()}
+
+      {/* 调整输出质量质量 */}
+      <div className={style.item} style={{ marginBottom: "10px" }}>
+        <RowStart className={style.title}>输出图片质量</RowStart>
+        <RowStart className={style.slider}>
+          <div className={style.sliderBox}>
+            <Slider
+              value={cstate.quality}
+              onChange={(quality) => {
                 setCstate({
                   ...cstate,
+                  quality,
                 });
               }}
             />
           </div>
-
-          {/* 调整缩放模式 */}
-          {showModeItem()}
-
-          {/* 调整输出质量质量 */}
-          <div className={style.item} style={{ marginBottom: "10px" }}>
-            <RowStart className={style.title}>输出图片质量</RowStart>
-            <RowStart className={style.slider}>
-              <div className={style.sliderBox}>
-                <Slider
-                  value={cstate.quality}
-                  onChange={(quality) => {
-                    setCstate({
-                      ...cstate,
-                      quality,
-                    });
-                  }}
-                />
-              </div>
-              <RowCenter>{Math.round(cstate.quality)}%</RowCenter>
-            </RowStart>
-          </div>
-
-          <RowEnd className={style.btns}>
-            <Button type="outline" onClick={onReset}>
-              重置选项
-            </Button>
-            <Button onClick={onApply}>立即应用</Button>
-          </RowEnd>
-        </div>
+          <RowCenter>{Math.round(cstate.quality)}%</RowCenter>
+        </RowStart>
       </div>
-    );
-  }
 
-  return null;
+      <RowEnd className={style.btns}>
+        <Button type="outline" onClick={onReset}>
+          重置选项
+        </Button>
+        <Button onClick={() => onApply?.(cstate)}>立即应用</Button>
+      </RowEnd>
+    </div>
+  );
 });
